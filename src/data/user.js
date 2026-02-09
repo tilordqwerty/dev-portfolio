ecd4a66f0501bb1d5437d219da192316271b3c56
elgabrielcisneros <tilordqwertydev3@gmail.com>
2026-02-08 22:46:20 -0400

    update cv path

--- src/data/user.js
+++ src/data/user.js
@@ -7,7 +7,7 @@ const INFO = {
 	},
 
 	socials: {
-		cv: "https://drive.google.com/file/d/1C7_OSkxxtJbdz5hT38WvpTYfOh5lN-TM/view?usp=drive_link",
+		cv: "https://drive.google.com/file/d/15Gjc7EIBBzkpoo4tx7l3pTsB3J2p1hoT/view?usp=drive_link",
 		github: "https://github.com/elgabrielcisneros/elgabrielcisneros",
 		linkedin: "https://www.linkedin.com/in/gabriel-cisneros-3216a12bb",
 		instagram: "https://instagram.com/gabo_cis",
